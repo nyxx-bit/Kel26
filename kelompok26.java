41f681f7468472ab0b5d6549c9a95a79694663e8
nyxx-bit <alkhawarismi989@gmail.com>
2020-04-04 11:10:27 +0700

    Praktikum Modul 4

---
+++ kelompok26.java
@@ -0,0 +1,9 @@
+public class kelompok26 {
+	public static void main(String[] args) {
+		pendefinisi objek = new pendefinisi();
+		objek.greeting();
+		objek.kelompok("Kelompok 26");
+		String print = objek.kenalan("Alkha", "Mengaji");
+		System.out.println(print);
+	}
+}
